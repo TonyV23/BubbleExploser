7dccb5f4e937ed506379554062905fd75a09b5b2
viper23 <htonnycarlos@gmail.com>
2021-12-07 14:02:11 +0200

    last fucking commit

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const countBubbleDisplay = document.querySelector("h3");
+let counter = 0;
+
 const bubbleMaker = () =>{
     //create the bubble
     const bubble = document.createElement("span");
@@ -16,7 +19,14 @@ const bubbleMaker = () =>{
     // set the randomly direction of bubble
     const plusMinus = Math.random() > 0.5 ? 1 : -1;
     bubble.style.setProperty("--left", Math.random() * 100 * plusMinus + "%");
+    
+    // remove the bubble when it's clicked on
+    bubble.addEventListener("click", ()=>{
+        countBubbleDisplay.textContent = counter ++;
+        bubble.remove(); 
+    }); 
+    // set the auto-desctruction of the bubble
+    setTimeout(() => {bubble.remove();}, 8000);
 };
-setInterval(() => {
-   bubbleMaker(); 
-}, 300);
+// setting time pushing bubble
+setInterval(()=>{bubbleMaker();},300);
